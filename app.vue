377bb42c00bf6c09e887475076ac731c4d00573c
Katya Shcherbakova <katerina.shcherbakova.003@gmail.com>
2024-12-11 13:18:43 +0200

    Initial commit

---
+++ app.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+      <v-container fluid>
+        <v-row>
+          <v-col cols="12" md="4">
+            <TaskColumn status="TODO" />
+          </v-col>
+          <v-col cols="12" md="4">
+            <TaskColumn status="In Progress" />
+          </v-col>
+          <v-col cols="12" md="4">
+            <TaskColumn status="Done" />
+          </v-col>
+        </v-row>
+      </v-container>
+  </v-app>
+</template>
+
+<script setup>
+import TaskColumn from '@/components/TaskColumn.vue';
+</script>
---
+++ components/TaskColumn.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-card>
+        <v-card-title>{{ status }}</v-card-title>
+    </v-card>
+</template>
+
+<script setup>
+const props = defineProps({
+    status: {
+        type: String,
+        required: true,
+    },
+});
+
+</script>
